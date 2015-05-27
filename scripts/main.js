a7b28e0476832d530a3f5ef6959bbce9640b8c62
edwardsCam <cedwards3054@gmail.com>
2015-05-27 15:32:26 -0600

    Sort by angle

--- scripts/main.js
+++ scripts/main.js
@@ -16,12 +16,15 @@ $(document).ready(function() {
         Solve();
     });
 
-    $("#canvas").click(function(e){
+    $("#canvas").click(function(e) {
         canoffset = $(canvas).offset();
         var mouse_x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(canoffset.left);
         var mouse_y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(canoffset.top) + 1;
-        points.push({x:mouse_x, y:mouse_y});
-     });
+        points.push({
+            x: mouse_x,
+            y: mouse_y
+        });
+    });
 
     var canvas = document.getElementById('canvas');
     canvas.style.border = "red 2px solid";
@@ -32,30 +35,26 @@ $(document).ready(function() {
 });
 
 function wipeCanvas() {
-    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function MainLoop() {
-
     Render();
-
     requestAnimationFrame(MainLoop);
 }
 
 function Render() {
-
     wipeCanvas();
-
-    for (var i = 0; i < points.length; i++){
+    for (var i = 0; i < points.length; i++) {
         var p = points[i];
         ctx.fillRect(p.x - 2, p.y - 2, 4, 4);
     }
-
 }
 
 function Solve() {
     var center = GetCenterOfMass();
     GetAngles(center);
+    SortByAngle();
 }
 
 function GetCenterOfMass() {
@@ -67,8 +66,8 @@ function GetCenterOfMass() {
         sumx += points[i].x;
         sumy += points[i].y;
     }
-    c.x = sumx/n;
-    c.y = sumy/n;
+    c.x = sumx / n;
+    c.y = sumy / n;
     return c;
 }
 
@@ -77,6 +76,18 @@ function GetAngles(center) {
         var p = points[i];
         var dx = p.x - center.x;
         var dy = p.y - center.y;
-        p.a = Math.atan2(dy,dx);
+        p.a = Math.atan2(dy, dx);
     }
+}
+
+function SortByAngle() {
+    points.sort(CompareByAngle);
+}
+
+function CompareByAngle(p1, p2) {
+    if (p1.a < p2.a)
+        return -1;
+    if (p1.a > p2.a)
+        return 1;
+    return 0;
 }
